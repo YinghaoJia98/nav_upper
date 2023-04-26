8acd689c0cb3ab63a0e5ba6d362171454b22a26a
yinghaojia <yinghaojia@163.com>
2023-04-26 20:12:20 +0800

    20230426

--- src/nav_upper.cpp
+++ src/nav_upper.cpp
@@ -2,7 +2,7 @@
 NavManager::NavManager(const ros::NodeHandle &nh, const ros::NodeHandle &nh_private)
     : nh_(nh), nh_private_(nh_private)
 {
-    ROS_INFO("Hello, world.");
+    // ROS_INFO("Hello, world.");
     initialize();
     setupTimer();
 }
@@ -12,7 +12,7 @@ void NavManager::initialize()
     std::string CostMap_topic_;
     nh_.param<std::string>("/nav_upper/nav_manager_settings/CostMap_topic", CostMap_topic_,
                            std::string("/hello"));
-    std::cout << "The topic is " << CostMap_topic_ << std::endl;
+    // std::cout << "The topic is " << CostMap_topic_ << std::endl;
     CostMapSubscriber_ = nh_.subscribe(CostMap_topic_.c_str(), 1,
                                        &NavManager::LocalCostMapCallBack, this);
 }
@@ -26,7 +26,7 @@ void NavManager::setupTimer()
         double duration_UpdateTarget = 1.0 / (UpdateTargetFps_ + 0.00001);
         UpdateTargetTimer = nh_.createTimer(ros::Duration(duration_UpdateTarget),
                                             &NavManager::UpdateTargetTimerCallBack, this);
-        ROS_INFO("UpdateTargetFps_ is %f", UpdateTargetFps_);
+        // ROS_INFO("UpdateTargetFps_ is %f", UpdateTargetFps_);
     }
 }
 
